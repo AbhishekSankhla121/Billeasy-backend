bcf3c92dd66c452eddcb67232e2034d6047a6509
abhishek <abhisheksankhla121@gmail.com>
2025-05-20 22:32:48 +0000

    remove username unique in userSchema

--- Backend/Models/user.js
+++ Backend/Models/user.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
     email:    { type: String, required: true, unique: true },
     password: { 
       select: false,
